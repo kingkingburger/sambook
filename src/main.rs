cca71934fbd4edc08fa4ad69e02f3cf4d7435cfc
minho <dnjsalsgh123@gmail.com>
2026-08-20 10:13:44 +0900

    rust에서 json 출력하기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use std::{error::Error, fs::File, println};
+
+use serde_json::Value;
+
+fn decode_json() -> Result<(), Box<dyn Error>>{
+    
+    let file = File::open("raw_data/1_5000_20260719.json")?;
+    let value: Value = serde_json::from_reader(file)?;
+
+    println!("{}", serde_json::to_string_pretty(&value)?);
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    decode_json()?;
     println!("Hello, world!");
+    Ok(())
 }
