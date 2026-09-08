ee6d4bd364dbb4ea81e24d91ca2eec780f7a5c9c
minho <dnjsalsgh123@gmail.com>
2026-09-08 11:05:01 +0900

    원본 데이터에서 단어 추출 함수 아카이브행

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ use std::error::Error;
 
 use crate::get_3_word::get_3_word;
 
-mod extract_json_to_one_word;
 mod get_3_word;
-mod load_json;
-mod refine_word;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let picked = get_3_word()?;
