662a48816dd31fa6d5bbce03f01df1ff7df32126
minho <dnjsalsgh123@gmail.com>
2026-09-09 14:23:58 +0900

    discord로 3가지 단어 보내기 기능 구현

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::error::Error;
 
-use crate::get_3_word::get_3_word;
+use crate::{get_3_word::get_3_word, send_discord_alert::send_discord_alert};
 
 mod get_3_word;
+mod send_discord_alert;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = dotenvy::dotenv();
+
     let picked = get_3_word()?;
     for word in picked {
+        send_discord_alert(word.to_string()).await?;
         println!("{}", word);
     }
 
---
+++ src/send_discord_alert.rs
@@ -0,0 +1,18 @@
+use std::{error::Error, println};
+
+use reqwest::Client;
+use serde_json::json;
+
+pub async fn send_discord_alert(message: String) -> Result<(), Box<dyn Error>> {
+    let webhook_url = std::env::var("DISCORD_WEBHOOK_URL")?;
+    let client = Client::new();
+
+    let payload = json!({
+        "content": message,
+        "username": "sambook"
+    });
+
+    client.post(webhook_url).json(&payload).send().await?;
+
+    Ok(())
+}
