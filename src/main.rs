a75129dbbfce69dfc5087c34827c93638a50db24
minho <dnjsalsgh123@gmail.com>
2026-09-14 14:00:48 +0900

    실행시간 측정

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, println, time::Instant};
 
 use crate::{get_3_word::get_3_word, send_discord_alert::send_discord_alert};
 
@@ -8,9 +8,11 @@ mod send_discord_alert;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let _ = dotenvy::dotenv();
-
+    let start = Instant::now();
     // 3단어를 , 로 묶어서 1번에 보내기
     let picked = get_3_word()?;
+    let elapsed = start.elapsed();
+    println!("실행시간: {:?}", elapsed);
 
     let words = picked
         .iter()
@@ -19,7 +21,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .join(",");
 
     println!("{}", words);
+
+    let send_start = Instant::now();
     send_discord_alert(words).await?;
+    let send_elapsed = send_start.elapsed();
+    println!("실행시간: {:?}", send_elapsed);
 
     Ok(())
 }
